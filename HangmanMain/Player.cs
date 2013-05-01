cbcb04228894d024c12f7aff00efdc03e5b9502a
Dimitrina Zlatkova <didi.zlatkova93@gmail.com>
2013-05-01 18:12:20 +0300

    Added Player.cs and ScoreManager.cs

---
+++ HangmanMain/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HangmanMain
+{
+    public class Player
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private int mistakes;
+        public int Mistakes
+        {
+            get { return mistakes; }
+            set { mistakes = value; }
+        }
+
+        public Player(string name, int mistakes)
+        {
+            this.name = name;
+            this.mistakes = mistakes;
+        }
+    }
+}
---
+++ HangmanMain/ScoreManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HangmanMain
+{
+    public class ScoreManager
+    {
+        private static List<Player> topPlayers = new List<Player>();
+
+        public static void AddPlayerToScoreBoard(Player player)
+        {
+            if (topPlayers.Count < 5)
+            {
+                topPlayers.Add(player);
+            }
+            else
+            {
+                for (int i = 0; i < topPlayers.Count; i++)
+                {
+                    if (topPlayers[i].Mistakes > player.Mistakes)
+                    {
+                        topPlayers.Insert(i, player);
+                        topPlayers.RemoveAt(topPlayers.Count - 1);
+                        break;
+                    }
+                }
+            }
+            SortPlayers();
+        }
+
+        private static void SortPlayers()
+        {
+            // not sure if sorting works right
+            topPlayers.Sort((Player firstPlayer, Player secondPlayer) => firstPlayer.Mistakes.CompareTo(secondPlayer.Mistakes));
+            topPlayers.Sort((Player firstPlayer, Player secondPlayer) => secondPlayer.Name.CompareTo(firstPlayer.Name));
+        }
+
+    }
+}
