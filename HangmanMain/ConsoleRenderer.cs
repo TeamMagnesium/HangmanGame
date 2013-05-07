827d557a8e60f5ce1ca17740a839d2ac448cfc2b
Lubica Milanova <lubica21@abv.bg>
2013-05-07 22:43:02 +0300

    Added methods in ConsoleRenderer.cs

--- HangmanMain/ConsoleRenderer.cs
+++ HangmanMain/ConsoleRenderer.cs
@@ -9,6 +9,11 @@ namespace HangmanMain
     {
         public static void PrintWelcomeMessage()
         {
+            StringBuilder printMessage = new StringBuilder();
+            printMessage.Append("Welcome to \"Hangman\" game. Please try to guess my secret word\r\n");
+            printMessage.Append("Use 'top' to view the scoreboard, 'restar' to start a new game and 'exit' to quit the game.");
+
+            Console.WriteLine(printMessage.ToString());
         }
 
         public static void PrintUserWordMessage(string userWord)
@@ -17,22 +22,42 @@ namespace HangmanMain
 
         public static void PrintCorrectLetterMessage(int revealedLetters)
         {
+            StringBuilder printMessage = new StringBuilder();
+            printMessage.AppendFormat("Good job! You revealed {0} letter.", revealedLetters);
+
+            Console.WriteLine(printMessage.ToString());
         }
 
         public static void PrintIncorrectLetterMessage(char guessedLetter)
         {
+            StringBuilder printMessage = new StringBuilder();
+            printMessage.AppendFormat("Sorry! There are no unrevealed letters \"{0}\".", guessedLetter);
+
+            Console.WriteLine(printMessage.ToString());
         }
 
         public static void PrintWinningMessage(int mistakes)
         {
+            StringBuilder printMessage = new StringBuilder();
+            printMessage.AppendFormat("You won with {0} mistakes.", mistakes);
+
+            Console.WriteLine(printMessage.ToString());
         }
 
         public static void PrintCheatingMessage(int mistakes)
         {
+            StringBuilder printMessage = new StringBuilder();
+            printMessage.AppendFormat("You won with {0} mistakes but you have cheated. You are not allowed to enter unto the scoreboard.", mistakes);
+
+            Console.WriteLine(printMessage.ToString());
         }
 
         public static void PrintGetNameForScoreboard()
         {
+            StringBuilder printMessage = new StringBuilder();
+            printMessage.Append("Please enter your name for the top scoreboard:");
+
+            Console.Write(printMessage.ToString());
         }
 
         public static void PrintScoreboard(List<Player> topPlayers)
@@ -41,6 +66,8 @@ namespace HangmanMain
 
         public static void PrintIncorrectInputMessage(string message)
         {
+            StringBuilder printMessage = new StringBuilder();
+            printMessage.Append("Incorrect guess or command!");
         }
 
         public static void PrintExitMessage()
