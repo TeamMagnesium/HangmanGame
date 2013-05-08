e33aa3ab4d10862087223bd910e655a54b9adb85
Lubica Milanova <lubica21@abv.bg>
2013-05-08 22:06:01 +0300

    Added Unit Test for ConsoleRenderer.cs

--- TestHangmanGame/TestConsoleRenderer.cs
+++ TestHangmanGame/TestConsoleRenderer.cs
@@ -3,14 +3,42 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TestHangmanGame
 {
+    using System.IO;
+
     using HangmanMain;
 
     [TestClass]
     public class TestConsoleRenderer
     {
         [TestMethod]
-        public void TestWelcomeMessage()
+        public void ValidateConsoleExitMessage()
         {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                ConsoleRenderer.PrintExitMessage();
+
+                string expected = string.Format("Good bye!{0}", Environment.NewLine);
+                Assert.AreEqual(expected, sw.ToString());
+            }
         }
+
+        [TestMethod]
+        public void ValidateConsoleWelcomeMessage()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                ConsoleRenderer.PrintWelcomeMessage();
+
+                string expected =
+                    string.Format(
+                        "Welcome to \"Hangman\" game. Please try to guess my secret word\r\nUse 'top' to view the scoreboard,"
+                        + " 'restart' to start a new game and 'exit' to quit the game.{0}",
+                        Environment.NewLine);
+                Assert.AreEqual(expected, sw.ToString());
+            }
+        }
+
     }
 }
