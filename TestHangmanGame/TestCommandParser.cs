fb6d9694bc0dab4307a77ef9a5f92e27e21d7558
git12 <neshto123@gmail.com>
2013-05-15 16:38:01 +0300

    Added TestCommandParser. It contains tests for the methods in class CommandParser

---
+++ TestHangmanGame/TestCommandParser.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HangmanMain;
+
+namespace TestHangmanGame1
+{
+    [TestClass]
+    public class TestCommandParser
+    {
+        private static CommandParser parser;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            parser = new CommandParser();
+        }
+
+        [TestMethod]
+        public void CommandParser_CommandIsValid()
+        {
+            string command = "top";
+            string parsedCommand = parser.ParseCommand(command);
+            Assert.AreEqual(command, parsedCommand);
+        }
+
+        [TestMethod]
+        public void CommandParser_LetterIsValid()
+        {
+            string command = "a";
+            string parsedCommand = parser.ParseCommand(command);
+            Assert.AreEqual(command, parsedCommand);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CommandParser_CommandIsNotValid()
+        {
+            string command = "hello";
+            string parsedCommand = parser.ParseCommand(command);
+        }
+    }
+}
